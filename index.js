4953f8ed3f666f7593ae60a0e373ccf3319866f7
Ahmed Ali <ahmedalidope@gmail.com>
2024-10-29 08:52:07 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var divtag = document.getElementById('div')
+function handleclick(){
+var input= document.getElementById("input")
+var result = input.value
+divtag.innerHTML +=`<h1>${result}</h1>`
+input.value=""
+
+}
+
+function dlt() {
+    div.innerHTML = ""; 
+}
